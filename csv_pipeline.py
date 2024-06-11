0ca2b10f3bd4071c8c2df760ca56ebbf8ef2e94b
alvarodelser <a.fontecha.ser@gmail.com>
2024-06-11 16:23:36 +0200

    Update csv_pipeline.py

--- csv_pipeline.py
+++ csv_pipeline.py
@@ -105,7 +105,7 @@ def flatten_csv(folder_path):
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.csv') and file_name.startswith('export_CAN'):
             file_path = os.path.join(folder_path, file_name)
-            current_df = pd.read_csv(file_path, dtypes = dtypes)
+            current_df = pd.read_csv(file_path, dtype = dtypes)
             dfs.append(current_df)
     df = pd.concat(dfs, ignore_index=True)
 
