cf52f884f34856b702f8f0f98864e459878219ee
alvarodelser <a.fontecha.ser@gmail.com>
2024-06-18 09:59:01 +0200

    Updated csv-pipeline.py: Converters and columns to read added, simplified formatting, analysis of nulls

--- csv-pipeline.py
+++ csv-pipeline.py
@@ -9,6 +9,27 @@ import wget
 import zipfile
 import shutil
 import getpass
+
+#                               ------------ CONSTANTS -----------------
+FOLDER = "./temp/csv/"
+HOST = 'localhost'
+PORT = 9200
+INDEX = 'ted-csv'
+username = input("Enter ProCureSpot username: ")
+password = getpass.getpass(prompt="Enter ProCureSpot password: ")
+auth = (username, password)
+
+# Create the client with SSL/TLS enabled, but hostname verification disabled.
+client = OpenSearch(
+    hosts=[{'host': HOST, 'port': PORT}],
+    http_compress=True,  # enables gzip compression for request bodies
+    http_auth=auth,
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=True,
+    ssl_show_warn=False,
+)
+
 #                          ------------ FUNCTIONS -----------------
 
 # Function to download files from url into file_path
@@ -18,11 +39,6 @@ def download_file(url, file_path):
         print(f"Downloaded: {url}")
     except Exception as e:
         print(f"Failed to download {url}: {e}")
-
-def transform_id(id_csv): #Function to swap the ID_NOTICE_CAN field so it aligns with the one used in the xml format
-    year_part = str(id_csv)[:4]
-    id_xml = str(id_csv)[4:].zfill(6) + '-' + year_part
-    return id_xml
 
 # Extracts a .zip compressed file and deletes the compressed file
 def extract_file(file_path):
@@ -44,7 +60,6 @@ def extract_file(file_path):
     except Exception as e:
         print(f"Failed to extract {file_path}: {e}")
 
-
 def download_csv(save_folder):
     start_year = 2018
     end_year = 2023
@@ -54,44 +69,43 @@ def download_csv(save_folder):
         save_path = f"{save_folder}TED_AWARD_{year}.zip"
         download_file(download_url, save_path)
         extract_file(save_path)
+
+#Converter functions for reading csv
+def transform_id(id_csv): #Function to swap the ID_NOTICE_CAN field so it aligns with the one used in the xml format
+    year_part = str(id_csv)[:4]
+    id_xml = str(id_csv)[4:].zfill(6) + '-' + year_part
+    return id_xml
 def bool_converter(value):
-    if pd.isnull(value) or value =='' or pd.isna(value):  # Handle NaN or None values
-        return False  # or handle differently based on your case
-    elif value == 'Y':
+    if pd.isnull(value) or value =='':
+        return False
+    elif value == 'Y' or value == '1':
         return True
-    elif value == 'N':
+    elif value == 'N' or value == '0':
         return False
     else:
         raise ValueError(f"Unexpected value '{value}' found in the column.")
+def int_converter(value):
+    if pd.isnull(value) or value =='':
+        return -1
+    else:
+        return int(value)
 def lots_converter(value):
     if pd.isnull(value) or value == '' or pd.isna(value):
-        return 0  # Handle NaN, None, or empty string values (assuming 0 is a default value)
+        return 0
     elif value.isdigit():
         return int(value)
     else:
         raise ValueError(f"Unexpected value '{value}' found in the column.")
+def date_converter(value):
+    return datetime.strptime(value, '%d/%m/%y')
 
-def flatten_csv(folder_path):
-    columns_can_level = ['ID_NOTICE_CAN', 'TED_NOTICE_URL', 'YEAR', 'ID_TYPE', 'DT_DISPATCH', 'XSD_VERSION',
-                         'CANCELLED',
-                         'CORRECTIONS', 'B_MULTIPLE_CAE', 'CAE_NAME', 'CAE_NATIONALID', 'CAE_ADDRESS', 'CAE_TOWN',
-                         'CAE_POSTAL_CODE', 'CAE_GPA_ANNEX', 'ISO_COUNTRY_CODE', 'ISO_COUNTRY_CODE_GPA',
-                         'B_MULTIPLE_COUNTRY',
-                         'ISO_COUNTRY_CODE_ALL', 'CAE_TYPE', 'EU_INST_CODE', 'MAIN_ACTIVITY',
-                         'B_ON_BEHALF', 'B_INVOLVES_JOINT_PROCUREMENT', 'B_AWARDED_BY_CENTRAL_BODY', 'TYPE_OF_CONTRACT',
-                         'B_FRA_AGREEMENT', 'FRA_ESTIMATED', 'B_DYN_PURCH_SYST', 'CPV', 'MAIN_CPV_CODE_GPA',
-                         'B_GPA', 'GPA_COVERAGE', 'LOTS_NUMBER', 'VALUE_EURO', 'VALUE_EURO_FIN_1', 'VALUE_EURO_FIN_2',
-                         'TOP_TYPE',
-                         'B_ACCELERATED', 'OUT_OF_DIRECTIVES', 'B_ELECTRONIC_AUCTION', 'NUMBER_AWARDS']
+def read_csvs(folder_path): #Reads all yearly csv and concats them. Groups by CAN ID.
+    columns_can_level = ['ID_NOTICE_CAN', 'TED_NOTICE_URL', 'YEAR', 'ID_TYPE', 'DT_DISPATCH', 'XSD_VERSION', 'CANCELLED', 'CORRECTIONS', 'B_MULTIPLE_CAE', 'CAE_NAME', 'CAE_NATIONALID', 'CAE_ADDRESS', 'CAE_TOWN', 'CAE_POSTAL_CODE', 'CAE_GPA_ANNEX', 'ISO_COUNTRY_CODE', 'ISO_COUNTRY_CODE_GPA', 'B_MULTIPLE_COUNTRY', 'ISO_COUNTRY_CODE_ALL', 'CAE_TYPE', 'EU_INST_CODE', 'MAIN_ACTIVITY', 'B_ON_BEHALF', 'B_INVOLVES_JOINT_PROCUREMENT', 'B_AWARDED_BY_CENTRAL_BODY', 'TYPE_OF_CONTRACT', 'B_FRA_AGREEMENT', 'FRA_ESTIMATED', 'B_DYN_PURCH_SYST', 'CPV', 'MAIN_CPV_CODE_GPA', 'B_GPA', 'GPA_COVERAGE', 'LOTS_NUMBER', 'VALUE_EURO', 'VALUE_EURO_FIN_1', 'VALUE_EURO_FIN_2', 'TOP_TYPE', 'B_ACCELERATED', 'OUT_OF_DIRECTIVES', 'B_ELECTRONIC_AUCTION', 'NUMBER_AWARDS']
     dtypes = {
         'ID_NOTICE_CAN': 'str',
         'TED_NOTICE_URL': 'str',
-        'YEAR': 'int',
-        'ID_TYPE': 'str',
-        'DT_DISPATCH': 'str',  # Dates can be converted later if needed
         'XSD_VERSION': 'str',
         'CANCELLED': 'bool',
-        'CORRECTIONS': 'int',
         'CAE_NAME': 'str',
         'CAE_NATIONALID': 'str',
         'CAE_ADDRESS': 'str',
@@ -106,20 +120,19 @@ def flatten_csv(folder_path):
         'MAIN_ACTIVITY': 'str',
         'TYPE_OF_CONTRACT': 'str',
         'FRA_ESTIMATED': 'str',
-        'CPV': 'str',
-        'MAIN_CPV_CODE_GPA': 'str',
-        'GPA_COVERAGE': 'str',
         'VALUE_EURO': 'float',
         'VALUE_EURO_FIN_1': 'float',
         'VALUE_EURO_FIN_2': 'float',
-        'TOP_TYPE': 'str',
-        'OUT_OF_DIRECTIVES': 'bool'
+        'TOP_TYPE': 'str'
     }
     bool_cols = ['B_MULTIPLE_CAE', 'B_MULTIPLE_COUNTRY', "B_ON_BEHALF", "B_INVOLVES_JOINT_PROCUREMENT",
                  "B_AWARDED_BY_CENTRAL_BODY", "B_FRA_AGREEMENT", "B_DYN_PURCH_SYST", "B_GPA", "B_ACCELERATED",
-                 "B_ELECTRONIC_AUCTION"]
+                 "B_ELECTRONIC_AUCTION", "OUT_OF_DIRECTIVES"]
+    int_cols = ['GPA_COVERAGE', 'CPV', 'CORRECTIONS', 'ID_TYPE', 'YEAR', 'MAIN_CPV_CODE_GPA']
     converters = {col: bool_converter for col in bool_cols}
+    converters.update({col: int_converter for col in int_cols})
     converters['LOTS_NUMBER'] = lots_converter
+    converters['DT_DISPATCH'] = date_converter
     dfs = []
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.csv') and file_name.startswith('export_CAN'):
@@ -131,74 +144,13 @@ def flatten_csv(folder_path):
     df_flat = df.groupby('ID_NOTICE_CAN').first()
     df_flat.index = df_flat.index.to_series().apply(transform_id)
     return df_flat
-def parse_date(date_str):
-    formats = ['%d-%b-%y', '%d-%m-%y', '%d/%m/%y']  # Add more formats if needed
-    for fmt in formats:
-        try:
-            return datetime.strptime(date_str, fmt)
-        except ValueError:
-            continue
-    raise ValueError("Date format not recognized: {}".format(date_str))  # Print the problematic value
-
-
-def format_fields(df_chunk):
-    float_cols = ["VALUE_EURO", "VALUE_EURO_FIN_1", "VALUE_EURO_FIN_2"]
-    for col in float_cols:
-        df_chunk.loc[:, col] = pd.to_numeric(df_chunk[col], errors='coerce')
-        df_chunk.loc[:, col].fillna(-1, inplace=True)
-
-    # Apply the parse_date function to the 'DT_DISPATCH' column
-    df_chunk.loc[:, 'DT_DISPATCH'] = df_chunk['DT_DISPATCH'].apply(parse_date)
-
-    bool_cols = ["CANCELLED", "B_MULTIPLE_CAE", "B_MULTIPLE_COUNTRY", "B_INVOLVES_JOINT_PROCUREMENT",
-                 "B_AWARDED_BY_CENTRAL_BODY", "B_FRA_AGREEMENT", "B_DYN_PURCH_SYST", "B_ELECTRONIC_AUCTION",
-                 "B_ON_BEHALF", "B_GPA", "B_ACCELERATED", "OUT_OF_DIRECTIVES"]
-    for col in bool_cols:
-        df_chunk.loc[:, col].replace({0: False, 1: True, 'N': False, 'Y': True}, inplace=True)
-        df_chunk.loc[:, col].fillna(False, inplace=True)
-
-    null_cols = ["ISO_COUNTRY_CODE_ALL", "GPA_COVERAGE", "XSD_VERSION", "TOP_TYPE", "TED_NOTICE_URL", "CAE_GPA_ANNEX",
-                 "CAE_POSTAL_CODE", "CAE_NATIONALID", "CAE_ADDRESS", "CAE_TOWN", "MAIN_ACTIVITY", "EU_INST_CODE",
-                 "TYPE_OF_CONTRACT", "ISO_COUNTRY_CODE_GPA", "GPA_COVERAGE"]
-    for col in null_cols:
-        df_chunk.loc[:, col].fillna("None", inplace=True)
-
-    df_chunk.loc[:, 'MAIN_CPV_CODE_GPA'].fillna("0", inplace=True)
-    df_chunk.loc[:, "FRA_ESTIMATED"].fillna("No", inplace=True)
-    df_chunk.loc[:, "LOTS_NUMBER"].fillna(0, inplace=True)
-
-    return df_chunk
-
-#                               ------------ CONSTANTS -----------------
-
-host = 'localhost'
-port = 9200
-index = 'ted-csv'
-username = input("Enter ProCureSpot username: ")
-password = getpass.getpass(prompt="Enter ProCureSpot password: ")
-auth = (username, password)  # For testing only. Don't store credentials in code.
-ca_certs_path = '/full/path/to/root-ca.pem'  # Provide a CA bundle if you use intermediate CAs with your root CA.
-
-# Create the client with SSL/TLS enabled, but hostname verification disabled.
-client = OpenSearch(
-    hosts=[{'host': host, 'port': port}],
-    http_compress=True,  # enables gzip compression for request bodies
-    http_auth=auth,
-    use_ssl=True,
-    verify_certs=False,
-    ssl_assert_hostname=True,
-    ssl_show_warn=False,
-)
-
-folder = "./temp/csv/"  #"/home/procure/data/ted/"
-
 
 #                               ------------ CODE -----------------
 
-#download_csv(folder)
-if not os.path.exists(folder):
-    os.makedirs(folder)
-df = flatten_csv(folder)
+#download_csv(FOLDER)
+if not os.path.exists(FOLDER):
+    os.makedirs(FOLDER)
+df = read_csvs(FOLDER)
 lines = df.shape[0]
 columns = df.columns
 
@@ -213,12 +165,11 @@ for i in range(0, iters):
 
     print('Uploading lines ' + str(start_line) + ' to ' + str(end_line))
     df_chunk = df.iloc[start_line:end_line + 1]
-    df_chunk = format_fields(df_chunk)
     # Prepare a list of actions for the bulk API
     actions = [
         {
             "_op_type": "index",
-            "_index": index,
+            "_index": INDEX,
             "_id": id_doc,
             **{f"{col_name}": doc[col_name] for col_name in columns}
         }
@@ -226,7 +177,7 @@ for i in range(0, iters):
     ]
     # Use the bulk API to index the documents
     try:
-        success, failed = helpers.bulk(client, actions, index=index, raise_on_error=True, refresh=True)
+        success, failed = helpers.bulk(client, actions, index=INDEX, raise_on_error=True, refresh=True)
         #Logging
         successful_ids = {action['_id'] for action in actions}
         failed_ids = {failure['index']['_id'] for failure in failed}
@@ -260,6 +211,6 @@ for i in range(0, iters):
             logs.append(log_entry)
     except Exception as e:
         print(f"Error during bulk indexing: {e}")
-shutil.rmtree(folder)
+shutil.rmtree(FOLDER)
 logs_df = pd.concat(logs, ignore_index=True)
 logs_df.to_csv("./logs/csv-ingestion.csv", index=False)
