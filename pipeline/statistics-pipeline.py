c17b7b813f90e6d1906fcd95fb23ef4dd52dd42c
alvarodelser <a.fontecha.ser@gmail.com>
2024-07-03 13:28:30 +0200

    Reindexing statistics. Formatting numbers

--- pipeline/statistics-pipeline.py
+++ pipeline/statistics-pipeline.py
@@ -114,8 +114,7 @@ for csv_file in stat_files:
     # Apply transformation directly using applymap
     num_columns_existing = [col for col in num_cols if col in df.columns]
     if num_columns_existing:
-        try:
-            df[num_columns_existing] = df[num_columns_existing].apply(convert_float)
+        df[num_columns_existing] = df[num_columns_existing].apply(convert_float)
 
     print(df.isna().sum().sum())
     columns_to_drop = ['DATAFLOW', 'Health care provider', 'Financing scheme', 'UNIT_MEASURE']
