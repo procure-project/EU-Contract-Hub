6361e89d19970e3e7d6a8cee6bd67a23bd26f67b
alvarodelser <a.fontecha.ser@gmail.com>
2024-08-26 11:21:51 +0200

    Processing Pipeline v2: Fixed CPV Code error, new fields: lot, contractor info, updated CA info, awards and criteria. Processing restricted to legacy xml for now. Uploads to new (at the moment dynamic index) procure_v2.

--- pipeline/processing-pipeline.py
+++ pipeline/processing-pipeline.py
@@ -3,46 +3,46 @@ import json
 import concurrent.futures
 from opensearchpy import OpenSearch, helpers
 import pandas as pd
-from deep_translator import GoogleTranslator
+#from deep_translator import GoogleTranslator
 import getpass
 
 
-translator = GoogleTranslator(source='auto', target='english')
-def translate_title_batch(titles):
-    return translator.translate_batch(titles)
-
-
-def translate_description_batch(descriptions):
-    all_lines = []
-    line_mapping = []
-
-    # Split descriptions into lines and keep track of line positions
-    for i, description in enumerate(descriptions):
-        description_split = description.splitlines()
-        all_lines.extend(description_split)
-        line_mapping.append((i, len(description_split)))
-
-    # Translate all lines in batch
-    translated_lines = translator.translate_batch(all_lines)
-
-    # Reconstruct descriptions from translated lines
-    translated_descriptions = []
-    line_index = 0
-    for doc_index, num_lines in line_mapping:
-        translated_description = "\n".join(translated_lines[line_index:line_index + num_lines])
-        translated_descriptions.append(translated_description)
-        line_index += num_lines
-
-    return translated_descriptions
+#translator = GoogleTranslator(source='auto', target='english')
+# def translate_title_batch(titles):
+#     return translator.translate_batch(titles)
+#
+#
+# def translate_description_batch(descriptions):
+#     all_lines = []
+#     line_mapping = []
+#
+#     # Split descriptions into lines and keep track of line positions
+#     for i, description in enumerate(descriptions):
+#         description_split = description.splitlines()
+#         all_lines.extend(description_split)
+#         line_mapping.append((i, len(description_split)))
+#
+#     # Translate all lines in batch
+#     translated_lines = translator.translate_batch(all_lines)
+#
+#     # Reconstruct descriptions from translated lines
+#     translated_descriptions = []
+#     line_index = 0
+#     for doc_index, num_lines in line_mapping:
+#         translated_description = "\n".join(translated_lines[line_index:line_index + num_lines])
+#         translated_descriptions.append(translated_description)
+#         line_index += num_lines
+#
+#     return translated_descriptions
 
 
 # Function to apply parallel translation to the DataFrame
-def batch_translate(df):
-    title_translations = translate_title_batch(df['Title'].tolist())
-    description_translations = translate_description_batch(df['Description'].tolist())
-    df['Title (Translation)'] = title_translations
-    df['Description (Translation)'] = description_translations
-    return df
+# def batch_translate(df):
+# #     title_translations = translate_title_batch(df['Title'].tolist())
+# #     description_translations = translate_description_batch(df['Description'].tolist())
+# #     df['Title (Translation)'] = title_translations
+# #     df['Description (Translation)'] = description_translations
+# #     return df
 
 def cpv_list_contains_healthcare_cpvs(cpv_list):
     health_cpv_list = [33600000,
@@ -51,14 +51,13 @@ def cpv_list_contains_healthcare_cpvs(cpv_list):
                        33141000, 33141420,
                        85100000,
                        35113400, 18143000]
-    health_prefixes = [str(i).rstrip('0') for i in health_cpv_list]
+    health_prefixes = [str(i).zfill(8).rstrip('0') for i in health_cpv_list]
     if isinstance(cpv_list, int):# If cpv_list is a single integer, convert it to a list
         cpv_list = [cpv_list]
-    return any(any(str(cpv).lstrip('0').startswith(prefix) for prefix in health_prefixes) for cpv in cpv_list)
+    return any(any(str(cpv).zfill(8).startswith(prefix) for prefix in health_prefixes) for cpv in cpv_list)
 def processing_scroll(df):
     # VALUE FILTERING
     df['Value'] = df['Value'].where((df['Value'] > 100) & (df['Value'] < 10 ** 10), -1)
-
     df["Healthcare CPV"] = df["CPV"].apply(cpv_list_contains_healthcare_cpvs)
 
 
@@ -68,6 +67,135 @@ def processing_scroll(df):
     #     print(e)
     return df
 
+def extract_lots(can):
+    lots = can.get("OBJECT_CONTRACT", {}).get("OBJECT_DESCR", [])
+    extracted_lots = []
+    if isinstance(lots, dict):
+        lots=[lots]
+    for lot in lots:
+        # Extract the title, short description, and lot number
+        lot_title = lot.get("TITLE", "-")
+        lot_short_descr = lot.get("SHORT_DESCR", "-")
+        lot_no = lot.get("LOT_NO", "-")
+        cpv = lot.get("CPV_MAIN", {}).get("CPV_CODE", {}).get("@CODE", "-")
+
+        # Extract the criteria and their weightings
+        ac_list = lot.get("AC", {})
+        criteria_list = []
+        if isinstance(ac_list, dict): #Handle if contractors is a list or dictionary. I do not know for sure if it can be a list
+            ac_list = [ac_list]
+
+        for ac in ac_list:
+            try:
+                criteria = {"Price": {"Weight": ac.get("AC_PRICE", {}).get("AC_WEIGHTING", 0)}}
+                ac_quality = ac.get("AC_QUALITY", []) if isinstance(ac.get("AC_QUALITY", []), list) else [ac.get("AC_QUALITY", {})]
+                if ac_quality:
+                    criteria["Quality"] = [{"Criterion": q.get("AC_CRITERION", "-"), "Weight": q.get("AC_WEIGHTING", 0)} for q in ac_quality]
+                ac_cost = ac.get("AC_COST", []) if isinstance(ac.get("AC_COST", []), list) else [ac.get("AC_COST", {})]
+                if ac_cost:
+                    criteria["Cost"] = [{"Criterion": q.get("AC_CRITERION", "-"), "Weight": q.get("AC_WEIGHTING", 0)} for q in ac_cost]
+                criteria_list.append(criteria)
+            except Exception as e:
+                print(f"Error extracting criteria: {e}")
+                criteria_list.append({"Price": {"Weight": 100}})
+        extracted_lots.append({
+            "Lot Number": lot_no,
+            "Title": lot_title,
+            "Short Description": lot_short_descr,
+            "Criteria": criteria_list,
+            "CPV Codes": cpv
+        })
+
+    return extracted_lots
+
+def extract_awarded_contracts(can):
+    aw_contracts = can.get("AWARD_CONTRACT", {})
+    if isinstance(aw_contracts, dict):  # Handle if contractors is a list or dictionary. I do not know for sure if it can be a list
+        aw_contracts = [aw_contracts]
+    awards = []
+    for aw_contract in aw_contracts:
+        aw_title = aw_contract.get("TITLE", "-")
+
+        awarded_lot = aw_contract.get("AWARDED_CONTRACT", {})
+        n_tenders = awarded_lot.get("TENDERS", {}).get("NB_TENDERS_RECEIVED", "0")
+        contractors = awarded_lot.get("CONTRACTORS", {}).get("CONTRACTOR", [])
+        if isinstance(contractors, dict): #Handle if contractors is a list or dictionary. I do not know for sure if it can be a list
+            contractors = [contractors]
+
+        contractors_info = []
+        for contractor in contractors:
+            c_address = contractor.get("ADDRESS_CONTRACTOR", {})
+            c_name = c_address.get("OFFICIALNAME", "-")
+            c_country = c_address.get("COUNTRY", {}).get("@VALUE", "-")
+            c_town = c_address.get("TOWN", "-")
+            c_postal_code = c_address.get("POSTAL_CODE", "-")
+            c_address_line = c_address.get("ADDRESS", "-")
+            c_email = contractor.get("E_MAIL", "-")
+            c_phone = c_address.get("PHONE", "-")
+            c_url = contractor.get("URL", "-")  # Note: not all contractor objects have URL
+            c_national_id = c_address.get("NATIONALID", "-")
+
+            contractor_info = {
+                "Name": c_name,
+                "National ID": c_national_id,
+                "Address": {
+                    "Country": c_country,
+                    "Town": c_town,
+                    "Postal Code": c_postal_code,
+                    "Address": c_address_line, },
+                "Contact": {
+                    "URL": c_url,
+                    "Email": c_email,
+                    "Phone": c_phone
+                }
+            }
+
+            contractors_info.append(contractor_info)
+
+        aw_info = {
+            "Awarded Contract Title": aw_title,
+            "Number of Tenders": n_tenders,
+            "Contractors": contractors_info
+        }
+        awards.append(aw_info)
+
+    return awards
+
+
+def extract_contracting_authority(can):
+    contracting_body = can.get("CONTRACTING_BODY", {})
+    address = contracting_body.get("ADDRESS_CONTRACTING_BODY", {})
+
+    ca_activity = contracting_body.get("CA_ACTIVITY", {}).get("@VALUE", "-") or contracting_body.get(
+        "CA_ACTIVITY_OTHER", "-")
+    ca_name = address.get("OFFICIALNAME", "-")
+    ca_url_general = address.get("URL_GENERAL", "-")
+    ca_country = address.get("COUNTRY", {}).get("@VALUE", "-")
+    ca_town = address.get("TOWN", "-")
+    ca_postal_code = address.get("POSTAL_CODE", "-")
+    ca_address = address.get("ADDRESS", "-")
+    ca_phone = address.get("PHONE", "-")
+    ca_email = address.get("E_MAIL", "-")
+    ca_ca_type = contracting_body.get("CA_TYPE", {}).get("@VALUE", "-") or contracting_body.get("CA_TYPE_OTHER", "-")
+    ca_national_id = address.get("NATIONALID", "-")
+
+    return [{
+        "Name": ca_name,
+        "National ID": ca_national_id,
+        "Activity": ca_activity,
+        "CA Type": ca_ca_type,
+        "Address": {
+            "Country": ca_country,
+            "Town": ca_town,
+            "Postal Code": ca_postal_code,
+            "Address": ca_address
+        },
+        "Contact": {
+            "URL": ca_url_general,
+            "Email": ca_email,
+            "Phone": ca_phone
+        }
+    }]
 
 
 # Initialize the OpenSearch client
@@ -116,44 +244,64 @@ while True:
     # Extract document IDs and corresponding field values from the current batch of results
     for hit in response["hits"]["hits"]:  # Processing and Extracting Info Document-wise
         doc_id = hit["_id"]
+        skip = False  # Temp variable to skip certain formats in processing phase
+
         if "CONTRACT_AWARD_NOTICE" in hit["_source"].keys(): ######## Processing for legacy XML ####################################
+            can = hit["_source"]["CONTRACT_AWARD_NOTICE"]
+            if isinstance(can, list):
+                can = can[0]
+            try:
+                title = can.get("OBJECT_CONTRACT", {}).get("TITLE", "-")
+                description = can.get("OBJECT_CONTRACT", {}).get("SHORT_DESCR", "-")
 
-            title = hit["_source"]["CONTRACT_AWARD_NOTICE"]["OBJECT_CONTRACT"]["TITLE"]
-            description = hit["_source"]["CONTRACT_AWARD_NOTICE"]["OBJECT_CONTRACT"]["SHORT_DESCR"]
+                cpv_data = hit["_source"]["CODED_DATA_SECTION"]["NOTICE_DATA"]["ORIGINAL_CPV"] # may be a list or a dictionary
+                if isinstance(cpv_data, list):
+                    cpv = [int(item["@CODE"]) for item in cpv_data]
+                    cpv_desc = [str(item["#text"]) for item in cpv_data]
+                else:
+                    cpv = int(cpv_data["@CODE"])
+                    cpv_desc = str(cpv_data["#text"])
 
-            cpv_data = hit["_source"]["CODED_DATA_SECTION"]["NOTICE_DATA"]["ORIGINAL_CPV"] # may be a list or a dictionary
-            if isinstance(cpv_data, list):
-                cpv = [int(item["@CODE"]) for item in cpv_data]
-                cpv_desc = [str(item["#text"]) for item in cpv_data]
-            else:
-                cpv = int(cpv_data["@CODE"])
-                cpv_desc = str(cpv_data["#text"])
+                health_cpv = False
 
-            health_cpv = False
+                country = hit["_source"]["CODED_DATA_SECTION"].get("NOTICE_DATA",{}).get("ISO_COUNTRY",{}).get("@VALUE","-")
 
-            country = hit["_source"]["CODED_DATA_SECTION"]["NOTICE_DATA"]["ISO_COUNTRY"]["@VALUE"]
-
-            ca_name = hit["_source"]["CONTRACT_AWARD_NOTICE"]["CONTRACTING_BODY"]["ADDRESS_CONTRACTING_BODY"][
-                "OFFICIALNAME"]
+                ca_data = extract_contracting_authority(can)
+                awards_data = extract_awarded_contracts(can)
+                lot_data = extract_lots(can)
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+                print(hit)
         else:          ############################################## Processing for eforms ##########################################
-            title = hit["_source"]["cac:ProcurementProject"]["cbc:Name"]
-            description = hit["_source"]["cac:ProcurementProject"]["cbc:Description"]
-
-            cpv_data = hit["_source"]["cac:ProcurementProject"]["cac:MainCommodityClassification"][
-                "cbc:ItemClassificationCode"]
-            if isinstance(cpv_data, list):
-                cpv = [int(item) for item in cpv_data]
-                cpv_desc = ['-' for item in cpv_data]
-            else:
-                cpv = str(cpv_data)
-                cpv_desc = '-'
-
-            health_cpv = False
-
-            country = hit["_source"]["cac:ProcurementProject"]["cac:RealizedLocation"]["cac:Address"]["cac:Country"][
-                "cbc:IdentificationCode"]
-
-            ca_name = hit["_source"]["cac:ContractingParty"]["cac:Party"]["cac:PartyName"]["cbc:Name"]
+            skip = True
+            # try:
+            #     title = hit["_source"]["cac:ProcurementProject"]["cbc:Name"]
+            #     description = hit["_source"]["cac:ProcurementProject"]["cbc:Description"]
+            #
+            #     cpv_data = hit["_source"]["cac:ProcurementProject"]["cac:MainCommodityClassification"]["cbc:ItemClassificationCode"]
+            #     if isinstance(cpv_data, list):
+            #         cpv = [int(item) for item in cpv_data]
+            #         cpv_desc = ['-' for item in cpv_data]
+            #     else:
+            #         cpv = str(cpv_data)
+            #         cpv_desc = '-'
+            #
+            #     health_cpv = False
+            #     locations = (hit["_source"]["cac:ProcurementProject"].get("cac:RealizedLocation",{}))
+            #     if isinstance(locations,list):
+            #         country = locations[0].get("cac:Address",{}).get("cac:Country",{}).get("cbc:IdentificationCode","-")
+            #     else:
+            #         country = locations.get("cac:Address", {}).get("cac:Country", {}).get("cbc:IdentificationCode", "-")
+            #     ca_data, lot_data, awards_data = ([],[],[])
+            # except AttributeError as e:
+            #     print(f"Skipping document due to error: {e}")
+            #     continue
+            # except ValueError as e:
+            #     print(f"Skipping document due to error: {e}")
+            #     continue
+            # except Exception as e:
+            #     print(f"An unexpected error occurred: {e}")
+            #     print(hit)
         try: ######################################################### Query for CSV data ################################################
             inner_hit = client.get(index="ted-csv", id=doc_id)
             value = inner_hit["_source"]["VALUE_EURO_FIN_2"]
@@ -181,15 +329,16 @@ while True:
             value = -1
             proc_route = "Unknown"
 
-            title_translated = "-" # No translation for now (too slow)
-            description_translated = "-"
+        title_translated = "-" # No translation for now (too slow)
+        description_translated = "-"
 
+        if skip == False:
             id_field_pairs.append((doc_id, title, title_translated, description, description_translated, cpv, cpv_desc,
-                                   health_cpv, country, value, proc_route, ca_name))
+                                   health_cpv, country, value, proc_route, ca_data, lot_data, awards_data))
     # Processing fields Scroll-level
     df = pd.DataFrame(id_field_pairs, columns=["Document ID", "Title", "Title (Translation)", "Description",
                                                "Description (Translation)", "CPV", "CPV Description", "Healthcare CPV",
-                                               "Country", "Value", "Procurement Route", "Contracting Authority Name"])
+                                               "Country", "Value", "Procurement Route", "Contracting Authority","Lots", "Awarded Contracts"])
 
     processing_scroll(df)
 
@@ -197,7 +346,7 @@ while True:
     actions = [
         {
             "_op_type": "index",
-            "_index": "procure",
+            "_index": "procure_v2",
             "_id": doc['Document ID'],
             **{f"{col_name}": doc[col_name] for col_name in df.columns if col_name != "Document ID"}
 
