d2882c386d5c6c592d9eb80c775146f9302d7ac3
AlvarodelSer <a.fontecha.ser@gmail.com>
2024-12-16 14:20:17 +0100

    Moved opensearch queries to extractormodule.py.

---
+++ pipeline/download_query.py
@@ -0,0 +1,30 @@
+from pipelinepackage.extractormodule import query_os
+import click
+import json
+
+
+def get_user_query():
+    # Provide a default query template
+    default_query = """{
+    "query": {
+        "match_all": {}
+    }
+}"""
+    # Open the default editor with the template
+    user_query = click.edit(default_query)
+
+    if user_query is None:
+        print("No query entered. Exiting.")
+        return None
+
+    # Validate or return the edited query
+    return user_query.strip()
+
+index = input("Select index:")
+# Example usage
+query = get_user_query()
+if query:
+    data = query_os(index, query)
+    data.to_csv("os_extraction.csv", index=False)
+    print("Results saved on os_extraction.csv for query:")
+    print(json.dumps(query, indent=4))
