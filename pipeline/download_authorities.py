648e1f326ed908b8c077b81a8c2b9d5c86878516
AlvarodelSer <a.fontecha.ser@gmail.com>
2024-12-16 13:16:25 +0100

    Moved opensearch queries to extractormodule.py.

--- pipeline/download_authorities.py
+++ pipeline/download_authorities.py
@@ -1,61 +1,11 @@
+from pipelinepackage.extractormodule import query_os
 
-from opensearchpy import OpenSearch, helpers
-import pandas as pd
-import getpass
+query_os(   index = "procure",
+            query = {
+                "_source": ["Lots"],
+                "query": {
+                    "match_all": {}  # Retrieve all documents
+                }
+            }
+         )
 
-# Opensearch client
-HOST = 'localhost'
-PORT = 9200
-
-username = input("Enter ProCureSpot username: ")
-password = getpass.getpass(prompt="Enter ProCureSpot password: ")
-auth = (username, password)
-# Create the client with SSL/TLS enabled, but hostname verification disabled.
-client = OpenSearch(
-    hosts=[{'host': HOST, 'port': PORT}],
-    http_compress=True,  # enables gzip compression for request bodies
-    http_auth=auth,
-    use_ssl=True,
-    verify_certs=False,
-    ssl_assert_hostname=True,
-    ssl_show_warn=False,
-)
-
-# Define your index and field
-index_name = "procure"
-# Define the query to retrieve all documents
-query = {
-    "query": {
-        "match_all": {
-        }
-    }
-}
-
-# Execute the initial search query to get the first batch of results
-response = client.search(
-    index=index_name,
-    body=query,
-    size=1000,  # Number of documents to retrieve per batch
-    scroll="1m"  # Keep the scroll window open for 1 minute
-)
-
-scroll_id = response["_scroll_id"]
-dfs=[]
-scr = 1
-while True:
-    response = client.scroll(scroll_id=scroll_id, scroll="1m")
-    id_field_pairs = []
-
-    for hit in response["hits"]["hits"]:  # Processing and Extracting Info Document-wise
-            doc_id = hit["_id"]
-            #CA = hit["_source"]["Contracting Authority"]
-            Lots = hit["_source"]["Lots"]
-            id_field_pairs.append((doc_id, Lots))
-
-    #df = pl.DataFrame(id_field_pairs, columns=["Document ID","Contracting Authorities"]) # Processing fields Scroll-level
-    df = pd.DataFrame(id_field_pairs, columns=["Document ID", "Lots"])
-    dfs.append(df)
-    if len(response["hits"]["hits"]) < 1000:
-        break
-final_df = pd.concat(dfs, ignore_index = True)
-final_df.to_csv("lots_extraction.csv", index=False)
--- pipeline/pipelinepackage/extractormodule.py
+++ pipeline/pipelinepackage/extractormodule.py
@@ -1,1 +1,65 @@
 #This module will group functions to extract csv from opensearch database.
+from opensearchpy import OpenSearch, helpers
+import pandas as pd
+from tqdm import tqdm
+import json
+
+def get_client():
+    HOST = 'localhost'
+    PORT = 9200
+
+    username = input("Enter ProCureSpot username: ")
+    password = getpass.getpass(prompt="Enter ProCureSpot password: ")
+    auth = (username, password)
+
+    # Create the client with SSL/TLS enabled, but hostname verification disabled.
+    return OpenSearch(
+        hosts=[{'host': HOST, 'port': PORT}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=True,
+        ssl_show_warn=False,
+    )
+
+
+
+def query_os(index, query):
+    client = get_client()
+    # Execute the initial search query to get the first batch of results
+    response = client.search(
+        index=index,
+        body=query,
+        size=10000,  # Number of documents to retrieve per batch
+        scroll="1m"  # Keep the scroll window open for 1 minute
+    )
+    scroll_id = response["_scroll_id"]
+    total_hits = response["hits"]["total"]["value"]  # Total number of hits
+
+    all_records = []  # List to store all documents
+    with tqdm(total=total_hits, desc="Fetching documents", unit="doc") as pbar:
+        while True:
+            # Extract document IDs and corresponding field values from the current batch of results
+            id_field_pairs = []
+            records = []
+            for hit in response["hits"]["hits"]:  # Processing and Extracting Info Document-wise
+                doc_id = hit["_id"]
+                doc = hit["_source"]
+                record = {"Document ID": doc_id, **doc}
+                all_records.append(record)
+
+            # Update tqdm progress
+            pbar.update(len(response["hits"]["hits"]))
+
+            # Stop scrolling if fewer than 10,000 hits in the current batch
+            if len(response["hits"]["hits"]) < 10000:
+                break
+
+            # Next Scroll
+            response = client.scroll(scroll_id=scroll_id, scroll="1m")
+            scroll_id = response["_scroll_id"]  # Update scroll ID for next batch
+    data = pd.DataFrame(all_records)
+    data.to_csv("os_extraction.csv", index=False)
+    print("Results saved on os_extraction.csv for query:")
+    print(json.dumps(query, indent=4))
