18ae3e651c5c5a84a81fbfb538071bda4b8955c7
AlvarodelSer <a.fontecha.ser@gmail.com>
2024-12-20 18:45:37 +0100

    Debugging eforms integration

--- pipeline/update_translations.py
+++ pipeline/update_translations.py
@@ -13,6 +13,5 @@ index2 = input("Select output index:")
 
 data = query_os(index1, translation_query, client)
 data.to_csv("translations_extraction.csv", index=False)
-print(data.loc[:5])
 actions = translation_upload_bulk_actions("translations_extraction.csv", index2)
-#process_bulk_batches(actions, client, batch_size=10000)
+process_bulk_batches(actions, client, batch_size=10000)
