84846a4e9383e6c9f7060ac21a9e6669e4fbf7d9
AlvarodelSer <a.fontecha.ser@gmail.com>
2025-01-29 10:53:55 +0100

    Adding new extraction query for large datasets. It extracts a complete index. Previous scripts ran into memory problems so this one extracts yearly csvs.

---
+++ pipeline/index_download_query.py
@@ -0,0 +1,28 @@
+from pipelinepackage.extractormodule import query_os
+import click
+import json
+import datetime
+
+index = int(input("Select index:"))
+
+current_year = datetime.datetime.now().year
+
+for y in range(2017, current_year + 1):
+    print('Extraction year ' + str(y))
+    start_date = datetime.date(y, 1, 1)
+    end_date = datetime.date(y, 12, 31)
+
+    query = {
+        "query": {
+            "range": {
+                "timestamp": {
+                    "gte": start_date,
+                    "lte": end_date
+                }
+            }
+        }
+    }
+
+    data = query_os(index, query)
+    if len(data) > 0:
+        data.to_csv(f"os_extraction_{start_date}_{end_date}.csv", index=False)
